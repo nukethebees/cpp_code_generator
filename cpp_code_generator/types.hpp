80de21784e7923634d4f7635ddb2bc4ce1655326
Matthew Love <matthewlove94@gmail.com>
2025-06-28 10:19:40 +0100

    Add member access methods

--- cpp_code_generator/types.hpp
+++ cpp_code_generator/types.hpp
@@ -4,6 +4,7 @@
 #include <expected>
 #include <string>
 #include <vector>
+#include <utility>
 
 // todo: enum + enum to string[_view]
 // todo: array with types as keys
@@ -23,6 +24,24 @@ enum class TokenType : uint8_t { END_OF_FILE };
 class Tokens {
   public:
     Tokens() = default;
+
+    // Member Access
+    template <typename Self>
+    auto&& token_chars(this Self&& self) {
+        return std::forward<Self>(self).token_chars_;
+    }
+    template <typename Self>
+    auto&& token_positions(this Self&& self) {
+        return std::forward<Self>(self).token_positions_;
+    }
+    template <typename Self>
+    auto&& token_lengths(this Self&& self) {
+        return std::forward<Self>(self).token_lengths_;
+    }
+    template <typename Self>
+    auto&& token_types(this Self&& self) {
+        return std::forward<Self>(self).token_types_;
+    }
   private:
     std::string token_chars_{""};
     std::vector<TokenPosition> token_positions_;
@@ -39,6 +58,20 @@ class Type {
         : name_(name)
         , parameters_(parameters)
         , parameter_vec_index_(parameter_vec_index) {}
+
+    // Member Access
+    template <typename Self>
+    auto&& name(this Self&& self) {
+        return std::forward<Self>(self).name_;
+    }
+    template <typename Self>
+    auto&& parameters(this Self&& self) {
+        return std::forward<Self>(self).parameters_;
+    }
+    template <typename Self>
+    auto&& parameter_vec_index(this Self&& self) {
+        return std::forward<Self>(self).parameter_vec_index_;
+    }
   private:
     TokenIndex name_;
     TypeIndex parameters_;
@@ -48,6 +81,16 @@ class Type {
 class Types {
   public:
     Types() = default;
+
+    // Member Access
+    template <typename Self>
+    auto&& types(this Self&& self) {
+        return std::forward<Self>(self).types_;
+    }
+    template <typename Self>
+    auto&& indexes(this Self&& self) {
+        return std::forward<Self>(self).indexes_;
+    }
   private:
     std::vector<Types> types_;
     std::vector<TypeIndex> indexes_;
@@ -59,6 +102,16 @@ class Member {
     Member(TokenIndex name, Type const& type)
         : name_(name)
         , type_(type) {}
+
+    // Member Access
+    template <typename Self>
+    auto&& name(this Self&& self) {
+        return std::forward<Self>(self).name_;
+    }
+    template <typename Self>
+    auto&& type(this Self&& self) {
+        return std::forward<Self>(self).type_;
+    }
   private:
     TokenIndex name_;
     Type type_;
@@ -70,6 +123,16 @@ class NamedArrayHeader {
     NamedArrayHeader(TokenIndex name, uint16_t type_index)
         : name_(name)
         , type_index_(type_index) {}
+
+    // Member Access
+    template <typename Self>
+    auto&& name(this Self&& self) {
+        return std::forward<Self>(self).name_;
+    }
+    template <typename Self>
+    auto&& type_index(this Self&& self) {
+        return std::forward<Self>(self).type_index_;
+    }
   private:
     TokenIndex name_;
     uint16_t type_index_;
@@ -80,6 +143,12 @@ class NamedArrayField {
     NamedArrayField() = delete;
     NamedArrayField(TokenIndex name)
         : name_(name) {}
+
+    // Member Access
+    template <typename Self>
+    auto&& name(this Self&& self) {
+        return std::forward<Self>(self).name_;
+    }
   private:
     TokenIndex name_{};
 };
@@ -87,6 +156,20 @@ class NamedArrayField {
 class NamedArrays {
   public:
     NamedArrays() = default;
+
+    // Member Access
+    template <typename Self>
+    auto&& types(this Self&& self) {
+        return std::forward<Self>(self).types_;
+    }
+    template <typename Self>
+    auto&& headers(this Self&& self) {
+        return std::forward<Self>(self).headers_;
+    }
+    template <typename Self>
+    auto&& fields(this Self&& self) {
+        return std::forward<Self>(self).fields_;
+    }
   private:
     std::vector<Type> types_;
     std::vector<NamedArrayHeader> headers_;
@@ -100,6 +183,20 @@ class UnionHeader {
         : members_offset_(members_offset)
         , members_length_(members_length)
         , name_(name) {}
+
+    // Member Access
+    template <typename Self>
+    auto&& members_offset(this Self&& self) {
+        return std::forward<Self>(self).members_offset_;
+    }
+    template <typename Self>
+    auto&& members_length(this Self&& self) {
+        return std::forward<Self>(self).members_length_;
+    }
+    template <typename Self>
+    auto&& name(this Self&& self) {
+        return std::forward<Self>(self).name_;
+    }
   private:
     uint16_t members_offset_;
     uint8_t members_length_;
@@ -109,6 +206,16 @@ class UnionHeader {
 class TaggedUnions {
   public:
     TaggedUnions() = default;
+
+    // Member Access
+    template <typename Self>
+    auto&& headers(this Self&& self) {
+        return std::forward<Self>(self).headers_;
+    }
+    template <typename Self>
+    auto&& members(this Self&& self) {
+        return std::forward<Self>(self).members_;
+    }
   private:
     std::vector<UnionHeader> headers_;
     std::vector<Member> members_;
@@ -123,6 +230,20 @@ class StructOfVectors {
 class Module {
   public:
     Module() = default;
+
+    // Member Access
+    template <typename Self>
+    auto&& tokens(this Self&& self) {
+        return std::forward<Self>(self).tokens_;
+    }
+    template <typename Self>
+    auto&& unions(this Self&& self) {
+        return std::forward<Self>(self).unions_;
+    }
+    template <typename Self>
+    auto&& named_arrays(this Self&& self) {
+        return std::forward<Self>(self).named_arrays_;
+    }
   private:
     Tokens tokens_;
     TaggedUnions unions_;
@@ -142,6 +263,24 @@ class Error {
         , source_position_(source_position)
         , source_length_(source_length)
         , type_(type) {}
+
+    // Member Access
+    template <typename Self>
+    auto&& message(this Self&& self) {
+        return std::forward<Self>(self).message_;
+    }
+    template <typename Self>
+    auto&& source_position(this Self&& self) {
+        return std::forward<Self>(self).source_position_;
+    }
+    template <typename Self>
+    auto&& source_length(this Self&& self) {
+        return std::forward<Self>(self).source_length_;
+    }
+    template <typename Self>
+    auto&& type(this Self&& self) {
+        return std::forward<Self>(self).type_;
+    }
   private:
     std::string message_;
     TokenPosition source_position_;
