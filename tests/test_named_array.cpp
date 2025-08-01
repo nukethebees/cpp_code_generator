1c91853d82925ba1104c505ccf9a24b30aa20fed
Matthew Love <matthewlove94@gmail.com>
2025-08-01 21:10:03 +0100

    Test comparison with int array

--- tests/test_named_array.cpp
+++ tests/test_named_array.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <format>
 #include <ostream>
 #include <string_view>
@@ -11,7 +12,19 @@
 #include "gtest_printing.hpp"
 #include "test_input.hpp"
 
-TEST(named_array, construct) {
+TEST(named_array, default_construct) {
     Foo const foo{};
     SUCCEED();
+}
+TEST(named_array, compare_int_array) {
+    Foo foo{};
+    foo[0] = 0;
+    foo[1] = 1;
+    foo[2] = 2;
+
+    std::array<int, 3> const is_eq{0, 1, 2};
+    std::array<int, 3> const is_ne{2, 1, 0};
+
+    EXPECT_EQ(foo, is_eq);
+    EXPECT_NE(foo, is_ne);
 }
