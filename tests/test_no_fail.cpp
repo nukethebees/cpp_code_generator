54ab867ee900fdab7c1d47b80a2a21a621bf2f87
Matthew Love <matthewlove94@gmail.com>
2025-08-02 12:13:48 +0100

    Print errors as well as the correct output

--- tests/test_no_fail.cpp
+++ tests/test_no_fail.cpp
@@ -13,28 +13,40 @@ TEST_P(NoFailTest, compile_file) {
 
     auto result{ccg::compile_file(param.test_name, param.file_input)};
 
-    if (result) {
-        auto const case_name{::testing::UnitTest::GetInstance()->current_test_case()->name()};
-        auto const test_name{::testing::UnitTest::GetInstance()->current_test_info()->name()};
-        static constexpr auto line1{"--------------------------------------------------------"sv};
-        static constexpr auto line2{"########################################################"sv};
+    auto const case_name{::testing::UnitTest::GetInstance()->current_test_case()->name()};
+    auto const test_name{::testing::UnitTest::GetInstance()->current_test_info()->name()};
+    static constexpr auto line_dash{"--------------------------------------------------------"sv};
+    static constexpr auto line_hash{"########################################################"sv};
 
-        *out_file << std::format(R"(
+    *out_file << std::format(R"(
 Test: {0} {1}
-{5}
+{2}
 [START_OF_INPUT]
-{3}[END_OF_INPUT]
-{2}
-[START_OF_OUTPUT]
-{4}[END_OF_OUTPUT]
-{2}
+{4}[END_OF_INPUT]
+{3}
 )",
-                                 case_name,
-                                 test_name,
-                                 line1,
-                                 param.file_input,
+                             case_name,
+                             test_name,
+                             line_dash,
+                             line_hash,
+                             param.file_input);
+
+    if (result) {
+        *out_file << std::format(R"(
+[START_OF_OUTPUT]
+{}[END_OF_OUTPUT]
+{}
+)",
                                  result->file(),
-                                 line2);
+                                 line_dash);
+    } else {
+        *out_file << std::format(R"(
+[START_OF_OUTPUT_ERROR]
+{}[END_OF_OUTPUT_ERROR]
+{}
+)",
+                                 result.error().message(),
+                                 line_dash);
     }
 
     ASSERT_TRUE(result) << result.error().message();
